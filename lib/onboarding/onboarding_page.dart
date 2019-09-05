2709d529edbe77970c365d337f4a5d36ae5908b4
Bhabin Khadka <bhabinkh@gmail.com>
2019-09-05 21:21:51 +0545

    Initial commit

---
+++ lib/onboarding/onboarding_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingPage extends StatefulWidget {
+  @override
+  _OnBoardingPageState createState() => _OnBoardingPageState();
+}
+
+class _OnBoardingPageState extends State<OnBoardingPage> {
+  int _currentIndex;
+
+  @override
+  void initState() {
+    _currentIndex = 0;
+    super.initState();
+  }
+
+  Widget _loginButton;
+  Widget _signUpButton;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          _localeMenu(),
+          GestureDetector(),
+          _OnBoardingPageItem(OnBoardingPageItemModel.getOnBoardingList()[0]),
+          _dots(),
+          Row(
+            children: <Widget>[
+              _loginButton,
+              _signUpButton,
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _localeMenu() {}
+
+  Widget _dots() {
+    return Row(
+      children: <Widget>[],
+    );
+  }
+}
+
+class _OnBoardingPageItem extends StatelessWidget {
+  final OnBoardingPageItemModel item;
+  final TextStyle _titleStyle = TextStyle();
+  final TextStyle _bodyStyle = TextStyle();
+
+  _OnBoardingPageItem(this.item);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Image.asset(this.item.imgURL),
+        Text(this.item.title, style: _titleStyle),
+        Text(this.item.body, style: _bodyStyle),
+      ],
+    );
+  }
+}
+
+class OnBoardingPageItemModel {
+  String imgURL;
+  String title;
+  String body;
+
+  OnBoardingPageItemModel(
+      {@required this.imgURL, @required this.title, @required this.body})
+      : assert(imgURL != null && title != null && body != null);
+
+  static List<OnBoardingPageItemModel> getOnBoardingList() {
+    return [
+      OnBoardingPageItemModel(
+          imgURL: 'test.svg',
+          title: 'Explore',
+          body: 'Helalkdjs flas aksdfj lasdkfj lasdjfas'),
+      OnBoardingPageItemModel(
+          imgURL: 'test.svg',
+          title: 'Book',
+          body: 'Helalkdjs flas aksdfj lasdkfj lasdjfas'),
+      OnBoardingPageItemModel(
+          imgURL: 'test.svg',
+          title: 'Access',
+          body: 'Helalkdjs flas aksdfj lasdkfj lasdjfas'),
+    ];
+  }
+}
