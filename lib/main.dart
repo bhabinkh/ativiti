cbe68d6b7fcf2b3872fe1ad000ec6b205a8bc3b8
Sailesh Dahal <saileshbro@gmail.com>
2019-09-06 19:01:30 +0545

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import 'package:ativiti/profile/profile.dart';
-=======
 import 'package:ativiti/application.dart';
->>>>>>> d0ade5d2775bc3a9d93c501d7b93e6bd8fe08727
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -14,16 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Profile(),
-    );
-=======
     return Application();
->>>>>>> d0ade5d2775bc3a9d93c501d7b93e6bd8fe08727
   }
 }
